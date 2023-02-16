2bc66529418c2e755a5bd3290e77c039fc1ffb0b
Le Phuoc thanh <lephuocthanh@MacBook-Air-cua-Le.local>
2023-02-16 19:23:40 +0700

    xong

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Authencation from './src/component/Authencation/Authencation';
+import Changeinfo from './src/component/ChangeInfo/Changinfo';
+import Main from './src/component/Main/Main';
+import OrderHistory from './src/component/OrderHistory/OrderHistory';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator
+        screenOptions={{headerShown:false}}>
+          <Stack.Screen
+          options={({navigation})=>{
+ 
+
+            headerLeft: ()=>{
+              <DrawerButton onPress={() => navigation.toggleDrawer()} />
+            }
+          }}
+    
+          name='Main'
+          component={Main}>
+          </Stack.Screen>
+          <Stack.Screen
+          name='Changeinfo'
+          component={Changeinfo}>
+          </Stack.Screen>
+          <Stack.Screen
+          name='Authencation'
+          component={Authencation}>
+          </Stack.Screen>
+          <Stack.Screen
+          name='OrderHistory'
+          component={OrderHistory}>
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+      
   );
 }
 
---
+++ src/component/Main/Main.js
@@ -0,0 +1,44 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+// create a component
+const Main = ({navigation}) => {
+    const goToAuthencation =()=>{
+        return navigation.navigate("Authencation");
+    }
+    const goToChangeInfo =()=>{
+        return navigation.navigate("Changeinfo");
+    }
+    const goToOrderHistory =()=>{
+        return navigation.navigate("OrderHistory");
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Main</Text>
+            <TouchableOpacity
+            onPress={goToAuthencation} ><Text>Go to authencation</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+            onPress={goToChangeInfo} ><Text>Go to ChangeInfo</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+            onPress={goToOrderHistory} ><Text>Go to OrderHistory</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2c3e50',
+    },
+});
+
+//make this component available to the app
+export default Main;
